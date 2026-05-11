3836ad3f9dfb4fa026e236d9672413ad667a7822
roryclear <roryclear.rc@gmail.com>
2026-05-11 12:15:02 +0100

    start inline preprocess

--- test.py
+++ test.py
@@ -217,6 +217,12 @@ def _sample(
 
     return input_ids
 
+def preprocess(proc, images, *args, **kwargs):
+    for kwarg_name in proc._valid_kwargs_names:
+        kwargs.setdefault(kwarg_name, getattr(proc, kwarg_name, None))
+
+    images = proc._prepare_image_like_inputs(images, **kwargs)
+    return proc._preprocess(images, *args, **kwargs)
 
 from transformers.models.qwen3_vl import Qwen3VLProcessor
 processor = Qwen3VLProcessor.from_pretrained("Qwen/Qwen3-VL-2B-Instruct")
@@ -240,7 +246,7 @@ for url, expected_output, prompt in zip(urls, expected_outputs, prompts):
     image = Image.open(BytesIO(requests.get(url).content)).convert("RGB")
     text_inputs = processor.tokenizer(prompt, return_tensors="pt", add_special_tokens=False)["input_ids"][0].tolist()
     
-    image_inputs = processor.image_processor.preprocess(images=image, return_tensors="pt")
+    image_inputs = preprocess(processor.image_processor, images=image, return_tensors="pt")
 
     merge_size = processor.image_processor.merge_size  # usually 2
     image_grid_thw = image_inputs["image_grid_thw"]  # [batch, 3] -> [t, h, w]
