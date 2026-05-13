67b39337aa29b78934b23b2f9c8b5e376e580128
roryclear <roryclear.rc@gmail.com>
2026-05-13 19:04:50 +0100

    inline more and add tiny stuff

--- test.py
+++ test.py
@@ -139,7 +139,68 @@ def forward(
 
     hidden_states = model.visual.patch_embed(pixel_values)
 
-    pos_embeds = fast_pos_embed_interpolate(model.visual, image_grid_thw)
+    grid_thw_list = image_grid_thw.tolist()
+    grid_ts = [row[0] for row in grid_thw_list]
+    grid_hs = [row[1] for row in grid_thw_list]
+    grid_ws = [row[2] for row in grid_thw_list]
+    device = model.visual.pos_embed.weight.device
+
+    idx_list = [[] for _ in range(4)]
+    weight_list = [[] for _ in range(4)]
+
+    for t, h, w in grid_thw_list:
+        h_idxs = torch.linspace(0, model.visual.num_grid_per_side - 1, h)
+        w_idxs = torch.linspace(0, model.visual.num_grid_per_side - 1, w)
+
+        h_idxs_floor = h_idxs.int()
+        w_idxs_floor = w_idxs.int()
+        h_idxs_ceil = (h_idxs.int() + 1).clip(max=model.visual.num_grid_per_side - 1)
+        w_idxs_ceil = (w_idxs.int() + 1).clip(max=model.visual.num_grid_per_side - 1)
+
+        dh = h_idxs - h_idxs_floor
+        dw = w_idxs - w_idxs_floor
+
+        base_h = h_idxs_floor * model.visual.num_grid_per_side
+        base_h_ceil = h_idxs_ceil * model.visual.num_grid_per_side
+
+        indices = [
+            (base_h[None].T + w_idxs_floor[None]).flatten(),
+            (base_h[None].T + w_idxs_ceil[None]).flatten(),
+            (base_h_ceil[None].T + w_idxs_floor[None]).flatten(),
+            (base_h_ceil[None].T + w_idxs_ceil[None]).flatten(),
+        ]
+
+        weights = [
+            ((1 - dh)[None].T * (1 - dw)[None]).flatten(),
+            ((1 - dh)[None].T * dw[None]).flatten(),
+            (dh[None].T * (1 - dw)[None]).flatten(),
+            (dh[None].T * dw[None]).flatten(),
+        ]
+
+        for i in range(4):
+            idx_list[i].extend(indices[i].tolist())
+            weight_list[i].extend(weights[i].tolist())
+
+    idx_tensor = torch.tensor(idx_list, dtype=torch.long, device=device)
+    weight_tensor = torch.tensor(weight_list, dtype=model.visual.pos_embed.weight.dtype, device=device)
+    pos_embeds = model.visual.pos_embed(idx_tensor) * weight_tensor[:, :, None]
+    patch_pos_embeds = pos_embeds[0] + pos_embeds[1] + pos_embeds[2] + pos_embeds[3]
+
+    patch_pos_embeds = patch_pos_embeds.split([h * w for h, w in zip(grid_hs, grid_ws)])
+
+    patch_pos_embeds_permute = []
+    merge_size = model.visual.config.spatial_merge_size
+    for pos_embed, t, h, w in zip(patch_pos_embeds, grid_ts, grid_hs, grid_ws):
+        pos_embed = pos_embed.repeat(t, 1)
+        pos_embed = (
+            pos_embed.view(t, h // merge_size, merge_size, w // merge_size, merge_size, -1)
+            .permute(0, 1, 3, 2, 4, 5)
+            .flatten(0, 4)
+        )
+        patch_pos_embeds_permute.append(pos_embed)
+    pos_embeds = torch.cat(patch_pos_embeds_permute)
+
+
     hidden_states = hidden_states + pos_embeds
 
     rotary_pos_emb = model.visual.rot_pos_emb(image_grid_thw)
@@ -421,69 +482,6 @@ def smart_resize(height, width, factor, min_pixels, max_pixels):
         w_bar = math.ceil(width * beta / factor) * factor
     return h_bar, w_bar
 
-def fast_pos_embed_interpolate(emb, grid_thw):
-    grid_thw_list = grid_thw.tolist()
-    grid_ts = [row[0] for row in grid_thw_list]
-    grid_hs = [row[1] for row in grid_thw_list]
-    grid_ws = [row[2] for row in grid_thw_list]
-    device = emb.pos_embed.weight.device
-
-    idx_list = [[] for _ in range(4)]
-    weight_list = [[] for _ in range(4)]
-
-    for t, h, w in grid_thw_list:
-        h_idxs = torch.linspace(0, emb.num_grid_per_side - 1, h)
-        w_idxs = torch.linspace(0, emb.num_grid_per_side - 1, w)
-
-        h_idxs_floor = h_idxs.int()
-        w_idxs_floor = w_idxs.int()
-        h_idxs_ceil = (h_idxs.int() + 1).clip(max=emb.num_grid_per_side - 1)
-        w_idxs_ceil = (w_idxs.int() + 1).clip(max=emb.num_grid_per_side - 1)
-
-        dh = h_idxs - h_idxs_floor
-        dw = w_idxs - w_idxs_floor
-
-        base_h = h_idxs_floor * emb.num_grid_per_side
-        base_h_ceil = h_idxs_ceil * emb.num_grid_per_side
-
-        indices = [
-            (base_h[None].T + w_idxs_floor[None]).flatten(),
-            (base_h[None].T + w_idxs_ceil[None]).flatten(),
-            (base_h_ceil[None].T + w_idxs_floor[None]).flatten(),
-            (base_h_ceil[None].T + w_idxs_ceil[None]).flatten(),
-        ]
-
-        weights = [
-            ((1 - dh)[None].T * (1 - dw)[None]).flatten(),
-            ((1 - dh)[None].T * dw[None]).flatten(),
-            (dh[None].T * (1 - dw)[None]).flatten(),
-            (dh[None].T * dw[None]).flatten(),
-        ]
-
-        for i in range(4):
-            idx_list[i].extend(indices[i].tolist())
-            weight_list[i].extend(weights[i].tolist())
-
-    idx_tensor = torch.tensor(idx_list, dtype=torch.long, device=device)
-    weight_tensor = torch.tensor(weight_list, dtype=emb.pos_embed.weight.dtype, device=device)
-    pos_embeds = emb.pos_embed(idx_tensor).to(device) * weight_tensor[:, :, None]
-    patch_pos_embeds = pos_embeds[0] + pos_embeds[1] + pos_embeds[2] + pos_embeds[3]
-
-    patch_pos_embeds = patch_pos_embeds.split([h * w for h, w in zip(grid_hs, grid_ws)])
-
-    patch_pos_embeds_permute = []
-    merge_size = emb.config.spatial_merge_size
-    for pos_embed, t, h, w in zip(patch_pos_embeds, grid_ts, grid_hs, grid_ws):
-        pos_embed = pos_embed.repeat(t, 1)
-        pos_embed = (
-            pos_embed.view(t, h // merge_size, merge_size, w // merge_size, merge_size, -1)
-            .permute(0, 1, 3, 2, 4, 5)
-            .flatten(0, 4)
-        )
-        patch_pos_embeds_permute.append(pos_embed)
-    patch_pos_embeds = torch.cat(patch_pos_embeds_permute)
-    return patch_pos_embeds
-
 def _preprocess(images):
     patch_size=16
     merge_size=2
@@ -545,7 +543,32 @@ def _preprocess(images):
 
 model = AutoModelForImageTextToText.from_pretrained("Qwen/Qwen3-VL-2B-Instruct")
 
-#print(model)
+from tinygrad import Tensor as tinyTensor
+from torch import Tensor
+from tinygrad.helpers import fetch
+from tinygrad.nn.state import safe_load, load_state_dict
+
+def to_tiny(x): return tinyTensor(x.detach().numpy())
+
+def to_torch(x): return Tensor(x.numpy())
+
+class blank: pass
+
+tiny_weights = safe_load(fetch(f'https://huggingface.co/Qwen/Qwen3-VL-2B-Instruct/resolve/main/model.safetensors'))
+
+print(model)
+print(model.model.visual.pos_embed)
+print(model.model.visual.pos_embed.weight.shape)
+
+tiny_model = blank()
+tiny_model.model = blank()
+tiny_model.model.visual = blank()
+tiny_model.model.visual.pos_embed = blank()
+tiny_model.model.visual.pos_embed.weight = tinyTensor.zeros(2304, 1024)
+load_state_dict(tiny_model, tiny_weights)
+
+#print(model.model.visual.pos_embed.bias) no bias
+#model.model.visual.pos_embed_tiny = to_tiny(model.model.visual.pos_embed)
 
 
 images = [Image.open(BytesIO(requests.get("https://img.wort.lu/public/luxemburg/vfka4n-picture-title-binary/alternates/ONE_ONE_256/Picture%20title%20binary").content)).convert("RGB"),
@@ -589,6 +612,7 @@ for image, expected_output, prompt in zip(images, expected_outputs, prompts):
     output = output.replace("<|im_end|>","") # todo hack
     print(output)
     assert output == expected_output
+
 
 
 
