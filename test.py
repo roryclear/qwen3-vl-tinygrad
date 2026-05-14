52dc92ac859b1b695bee792785f9955fc337a8e9
roryclear <roryclear.rc@gmail.com>
2026-05-14 14:41:04 +0100

    more tiny

--- test.py
+++ test.py
@@ -310,17 +310,18 @@ def forward(
 
         hidden_shape = (*input_shape, -1, model.model.language_model.layers[i].self_attn.head_dim)
 
-        query = model.model.language_model.layers[i].self_attn.q_proj(hidden_states).view(hidden_shape)
-        key = model.model.language_model.layers[i].self_attn.k_proj(hidden_states).view(hidden_shape)
+        hidden_states = to_tiny(hidden_states)
+        query = tiny_model.model.language_model.layers[i].self_attn.q_proj(hidden_states).view(hidden_shape)
+        key = tiny_model.model.language_model.layers[i].self_attn.k_proj(hidden_states).view(hidden_shape)
 
-        query = to_tiny(query)
-        key = to_tiny(key)
         query = tiny_model.model.language_model.layers[i].self_attn.q_norm(query).transpose(1, 2)
         key = tiny_model.model.language_model.layers[i].self_attn.k_norm(key).transpose(1, 2)
         query = to_torch(query)
         key = to_torch(key)
 
-        value = model.model.language_model.layers[i].self_attn.v_proj(hidden_states).view(hidden_shape).transpose(1, 2)
+        value = tiny_model.model.language_model.layers[i].self_attn.v_proj(hidden_states).view(hidden_shape).transpose(1, 2)
+        
+        value = to_torch(value)
 
         cos, sin = position_embeddings
         query = (query * cos) + (rotate_half(query) * sin)
@@ -347,8 +348,10 @@ def forward(
 
 
         attn_output = attn_output.reshape(*input_shape, -1).contiguous()
-        hidden_states = model.model.language_model.layers[i].self_attn.o_proj(attn_output)
+        attn_output = to_tiny(attn_output)
+        hidden_states = tiny_model.model.language_model.layers[i].self_attn.o_proj(attn_output)
 
+        hidden_states = to_torch(hidden_states)
         hidden_states = residual + hidden_states
         residual = hidden_states
         hidden_states = to_tiny(hidden_states)
@@ -607,6 +610,10 @@ if __name__ == "__main__":
     for i in range(len(model.model.language_model.layers)):
       tiny_model.model.language_model.layers.append(blank())
       tiny_model.model.language_model.layers[i].self_attn = blank()
+      tiny_model.model.language_model.layers[i].self_attn.q_proj = tiny_nn.Linear(2048, 2048, bias=False)
+      tiny_model.model.language_model.layers[i].self_attn.k_proj = tiny_nn.Linear(2048, 1024, bias=False)
+      tiny_model.model.language_model.layers[i].self_attn.v_proj = tiny_nn.Linear(2048, 1024, bias=False)
+      tiny_model.model.language_model.layers[i].self_attn.o_proj = tiny_nn.Linear(2048, 2048, bias=False)
       tiny_model.model.language_model.layers[i].self_attn.q_norm = Qwen3VLTextRMSNorm_tiny(size=128)
       tiny_model.model.language_model.layers[i].self_attn.k_norm = Qwen3VLTextRMSNorm_tiny(size=128)
       tiny_model.model.language_model.layers[i].input_layernorm = Qwen3VLTextRMSNorm_tiny(size=2048)
@@ -624,9 +631,9 @@ if __name__ == "__main__":
     images = [Image.open(BytesIO(requests.get("https://img.wort.lu/public/luxemburg/vfka4n-picture-title-binary/alternates/ONE_ONE_256/Picture%20title%20binary").content)).convert("RGB"),
             Image.open(BytesIO(requests.get("https://www.cartell.ie/car_check/wp-content/uploads/2012/03/Nissan-Micra-_4b.jpg").content)).convert("RGB"),
             Image.open("test_img.jpg").convert("RGB")]
-    expected_outputs = ["This is a Ferrari F40, a legendary sports car produced by Ferrari from 1987 to 1992. It is renowned for its sleek design and high performance, making it one of the most iconic cars in automotive history. The F40 was a groundbreaking vehicle that pushed the boundaries of automotive engineering and design, and it has become a symbol of speed, elegance, and innovation.",
-                        "The car in the image is the **Nissan Micra**, a compact car produced by Nissan. It was first introduced in 1990 and has been a popular choice in the UK and other markets.\n\nThe Nissan Micra is known for its affordability and practicality, making it a favorite among younger drivers and families. It was developed to meet the growing demand for small, efficient vehicles in the UK market.\n\nThe Micra has been produced in several generations, with the most recent being the **Nissan Micra (2006–2015)**, which was a significant model in the UK market. It",
-                        "A person wearing a grey hoodie is standing by the open driver's side door of a silver car."]
+    expected_outputs = ["This is a Ferrari F40, a legendary sports car produced by Ferrari from 1987 to 1992. It is renowned for its sleek design and high performance, making it one of the most iconic cars in automotive history.",
+                        "This is a Nissan Micra, a compact car produced by the Japanese automaker Nissan. The Micra is a popular and affordable car, known for its reliability and efficiency.\n\nThe Micra was first introduced in 1992 as a small, economical car. It was designed to be a practical and efficient vehicle for everyday use, and it quickly gained popularity in Japan and other markets.\n\nOver the years, the Micra has undergone several redesigns and updates. The most recent model, the 2019 version, is a compact hatchback that is known for its modern design, advanced features, and improved fuel efficiency.\n\n",
+                        "A person wearing a grey hoodie and light-colored pants is standing next to a silver car with the driver's side door open."]
 
     prompts = ["<|im_start|>user\n<|vision_start|><|image_pad|><|vision_end|>\nWhat car is this?<|im_end|>\n<|im_start|>assistant\n",
             "<|im_start|>user\n<|vision_start|><|image_pad|><|vision_end|>\nTell me the history of this car<|im_end|>\n<|im_start|>assistant\n",
@@ -661,9 +668,3 @@ if __name__ == "__main__":
         output = output.replace("<|im_end|>","") # todo hack
         print(output)
         assert output == expected_output
-
-
-
-
-
-
