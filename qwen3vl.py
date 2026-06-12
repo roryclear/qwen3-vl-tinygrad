90a95c3cb2a1ecd0d4ed0324453351cb3067f0f1
roryclear <roryclear.rc@gmail.com>
2026-06-12 12:14:07 +0100

    match transformers more

--- qwen3vl.py
+++ qwen3vl.py
@@ -239,7 +239,7 @@ class Qwen3VLVis():
     
     deepstack_feature_lists = []
     for i in range(len(self.v.blk)):
-      hidden_states = self.v.blk[i](hidden_states, cos, sin)
+      hidden_states = self.v.blk[i](hidden_states=hidden_states, position_embeddings=(cos, sin))
       if i in self.v.deepstack_idx: deepstack_feature_lists.append(self.v.deepstack[i](hidden_states))
 
     image_embeds = self.v.post_ln(hidden_states)
@@ -346,10 +346,11 @@ class Qwen3VisBlock:
     self.attn_qkv = nn.Linear(*weights["v.blk.0.attn_qkv.weight"].shape[::-1])
   
   #https://github.com/huggingface/transformers/blob/1316cd76c0ce328228e08d55dc257484961b074c/src/transformers/models/qwen3_vl/modeling_qwen3_vl.py#L280
-  def __call__(self, hidden_states, cos, sin):
+  def __call__(self, hidden_states: Tensor, position_embeddings: tuple[Tensor, Tensor]):
     hidden_states_input = self.ln1(hidden_states)
     # https://github.com/huggingface/transformers/blob/1316cd76c0ce328228e08d55dc257484961b074c/src/transformers/models/qwen3_vl/modeling_qwen3_vl.py#L186
     query, key, value = self.attn_qkv(hidden_states_input).reshape(hidden_states.shape[0], 3, self.num_heads, -1).permute(1, 0, 2, 3)
+    cos, sin = position_embeddings
     query, key = apply_rotary_pos_emb_vision(query, key, cos, sin)
     query = query.transpose(0, 1).unsqueeze(0)
     value = value.transpose(0, 1).unsqueeze(0)
